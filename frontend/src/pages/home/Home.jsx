03b6e6ff2ad3ea248ab8da722bc877c3ea8d3133
Samarthya Chabukswar <samarthyac1004@gmail.com>
2024-04-19 19:40:24 +0530

    mobile view fixed

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import Sidebar from '../../components/sidebar/Sidebar.jsx'
 import MessageContainer from '../../components/messages/MessageContainer.jsx'
 
 const Home = () => {
-   return <div className='border-solid border-2 border-cyan-800 flex sm:h-[450px] md:-[550px] rounded-lg overflow-hidden bg-white-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity'>
+   return <div className='border-solid border-2 border-cyan-800 flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
     <Sidebar />
     <MessageContainer /> 
     
